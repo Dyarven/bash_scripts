2ea7a0acc577577749a67ad4aa79c4d821b3eeeb
David Castro Vilas <81330265+Dyarven@users.noreply.github.com>
2022-04-06 15:59:41 +0200

    Update docker_auto.sh
    En Debian 10 ou anteriores hai que executar o seguinte antes do script:
    #apt-get --allow-releaseinfo-change update

--- docker_auto.sh
+++ docker_auto.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -euo pipefail
+set -e
 
 #docker e docker-compose instalación auto
 docker_autoinst() {
   #script inst docker linux param.
   curl -fsSL https://get.docker.com | bash
   #compose
-  $dockerPath=${dockerPath:-/usr/local/bin/docker-compose}
+  $dockerPath="/usr/local/bin/docker-compose"
   version=$( curl https://github.com/docker/compose/releases/latest | cut -d"/" -f 8 | cut -d"\"" -f 1 )
   version=$( expr ${version} )
   curl -fssL "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o $dockerPath
@@ -25,5 +25,3 @@ docker run -d --name=portainer --restart=always \
  -p 8000:8000 -p 9000:9000 \
  -v /var/run/docker.sock:/var/run/docker.sock \
  -v portainer_data:/data portainer/portainer-ce
-
-
