9100c8a68a69d74be0b50f7fd2b1de5acd2b45ad
David Castro Vilas <81330265+Dyarven@users.noreply.github.com>
2022-04-06 15:31:27 +0200

    Update docker_auto.sh

--- docker_auto.sh
+++ docker_auto.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-#executa un script de instalación automatizado de docker
-curl -sSL https://get.docker.com | bash
+#docker e docker-compose instalación auto
+docker_autoinst() {
+  #script inst docker linux param.
+  curl -fsSL https://get.docker.com | bash
+  #compose
+  $dockerPath=${dockerPath:-/usr/local/bin/docker-compose}
+  version=$( curl https://github.com/docker/compose/releases/latest | cut -d"/" -f 8 | cut -d"\"" -f 1 )
+  version=$( expr ${version} )
+  curl -fssL "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o $dockerPath
+}
 
-#instala docker compose
-$dockerPath=${dockerPath:-/usr/local/bin/docker-compose}
-curl -SL "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o $dockerPath
-
+docker_autoinst
 
 #activa o servizo docker e pon ao usuario no grupo
 chmod +x /usr/local/bin/docker-compose
@@ -20,4 +25,5 @@ docker run -d --name=portainer --restart=always \
  -p 8000:8000 -p 9000:9000 \
  -v /var/run/docker.sock:/var/run/docker.sock \
  -v portainer_data:/data portainer/portainer-ce
- 
+
+
