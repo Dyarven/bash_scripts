fba92dd676cee3909c8fa64795296ca93029cf1c
David <81330265+Dyarven@users.noreply.github.com>
2023-04-19 23:27:34 +0200

    Add files via upload

---
+++ fail2ban_logwatch_setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+apt update && apt install -y fail2ban logwatch
+
+#configuration
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+systemctl start fail2ban
+systemctl enable fail2ban
+
+LOGWATCH_CONF="/etc/logwatch/conf/logwatch.conf"
+LOGWATCH_CRON="/etc/cron.daily/00logwatch"
+#specify the discord channel ID as well on the webhook
+DISCORD="https://discord.com/api/webhooks/YOUR_DISCORD_WEBHOOK_URL_HERE"
+
+#logwatch alerts on discord
+sed -i "s/^MailTo.*/MailTo = /" $LOGWATCH_CONF
+sed -i "/^mailer =/a Format = html" $LOGWATCH_CONF
+sed -i "/^mailer =/a Output = stdout" $LOGWATCH_CONF
+sed -i "/^mailer =/a MailFrom = logwatch@$HOSTNAME" $LOGWATCH_CONF
+sed -i "/^mailer =/a MailSubject = Logwatch for $HOSTNAME" $LOGWATCH_CONF
+echo "MailTo = $DISCORD" >> $LOGWATCH_CONFIG
+
+#test webhook
+curl -H "Content-Type: application/json" -d '{"content":"Logwatch configurado correctamente"}' $DISCORD
+
+#add to crontab
+$LOGWATCH_CRON
+
+systemctl restart fail2ban
+systemctl restart logwatch
