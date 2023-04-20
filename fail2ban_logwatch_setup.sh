bb746eb7d4ea0e5363bec3707deebdb616741968
David <81330265+Dyarven@users.noreply.github.com>
2023-04-20 22:50:32 +0200

    Update fail2ban_logwatch_setup.sh

--- fail2ban_logwatch_setup.sh
+++ fail2ban_logwatch_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update && apt install -y fail2ban logwatch
 
-#configuration
+# Configuration
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 sed -i '/\[sshd\]/a enabled = true' /etc/fail2ban/jail.local
 sed -i '/\[sshd\]/a port = 3022' /etc/fail2ban/jail.local
@@ -11,23 +11,23 @@ sed -i '/\[sshd\]/a bantime = 2h' /etc/fail2ban/jail.local
 
 LOGWATCH_CONF="/etc/logwatch/conf/logwatch.conf"
 LOGWATCH_CRON="/etc/cron.daily/00logwatch"
-#specify the discord channel ID as well on the webhook
+# Specify the discord channel ID as well on the webhook
 DISCORD="https://discord.com/api/webhooks/xxx"
 
-#logwatch alerts on discord
+# Logwatch alerts on discord
 sed -i "s/^MailTo.*/MailTo = /" $LOGWATCH_CONF
 sed -i "/^mailer =/a Format = html" $LOGWATCH_CONF
 sed -i "/^mailer =/a Output = stdout" $LOGWATCH_CONF
 sed -i "/^mailer =/a MailFrom = logwatch@$HOSTNAME" $LOGWATCH_CONF
 sed -i "/^mailer =/a MailSubject = Logwatch en servidor $HOSTNAME" $LOGWATCH_CONF
-#only send critical alerts
+# Only send critical alerts
 sed -i "/^mailer =/a Threshold = 2" $LOGWATCH_CONF 
 echo "MailTo = $DISCORD" >> $LOGWATCH_CONF
 
-#test webhook
+# Test webhook
 curl -H "Content-Type: application/json" -d '{"content":"Logwatch configurado correctamente"}' $DISCORD
 
-#add to crontab
+# Add to crontab
 $LOGWATCH_CRON
 
 systemctl enable fail2ban && systemctl start fail2ban
