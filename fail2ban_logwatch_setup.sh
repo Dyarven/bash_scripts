d7928382290591b97c0ab3c537bf676e2026a87a
David <81330265+Dyarven@users.noreply.github.com>
2023-04-20 22:27:04 +0200

    Update fail2ban_logwatch_setup.sh

--- fail2ban_logwatch_setup.sh
+++ fail2ban_logwatch_setup.sh
@@ -9,9 +9,6 @@ sed -i '/\[sshd\]/a filter = sshd' /etc/fail2ban/jail.local
 sed -i '/\[sshd\]/a maxretry = 5' /etc/fail2ban/jail.local
 sed -i '/\[sshd\]/a bantime = 2h' /etc/fail2ban/jail.local
 
-systemctl start fail2ban
-systemctl enable fail2ban
-
 LOGWATCH_CONF="/etc/logwatch/conf/logwatch.conf"
 LOGWATCH_CRON="/etc/cron.daily/00logwatch"
 #specify the discord channel ID as well on the webhook
@@ -23,7 +20,7 @@ sed -i "/^mailer =/a Format = html" $LOGWATCH_CONF
 sed -i "/^mailer =/a Output = stdout" $LOGWATCH_CONF
 sed -i "/^mailer =/a MailFrom = logwatch@$HOSTNAME" $LOGWATCH_CONF
 sed -i "/^mailer =/a MailSubject = Logwatch en servidor $HOSTNAME" $LOGWATCH_CONF
-echo "MailTo = $DISCORD" >> $LOGWATCH_CONFIG
+echo "MailTo = $DISCORD" >> $LOGWATCH_CONF
 
 #test webhook
 curl -H "Content-Type: application/json" -d '{"content":"Logwatch configurado correctamente"}' $DISCORD
@@ -31,5 +28,7 @@ curl -H "Content-Type: application/json" -d '{"content":"Logwatch configurado co
 #add to crontab
 $LOGWATCH_CRON
 
-systemctl restart fail2ban
-systemctl restart logwatch
+systemctl enable fail2ban && systemctl start fail2ban
+systemctl enable logwatch && systemctl start logwatch
+
+
