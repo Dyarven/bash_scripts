d6435702da1810aa9d0d9bd37df292e73f9bddfa
David <81330265+Dyarven@users.noreply.github.com>
2023-06-01 04:34:31 +0200

    Update fail2ban_setup.sh

--- fail2ban_setup.sh
+++ fail2ban_setup.sh
@@ -7,10 +7,4 @@ cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 # Modify specific options in jail.local
 echo -e "[sshd]\nenabled = true\nport = 3022\nfilter = sshd\nmaxretry = 5\nbantime = 2h" >> /etc/fail2ban/jail.local
 
-# Specify the discord channel ID as well on the webhook
-DISCORD="https://discord.com/api/webhooks/xxx"
-
-# Test webhook
-curl -H "Content-Type: application/json" -d '{"content":"Configurado correctamente"}' $DISCORD
-
 systemctl enable fail2ban && systemctl start fail2ban
