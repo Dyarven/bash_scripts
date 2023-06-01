15331a7526dd6f115ed9dfe646863c3838c9454d
David <81330265+Dyarven@users.noreply.github.com>
2023-06-01 13:50:17 +0200

    Update fail2ban_ssh.sh

--- fail2ban_ssh.sh
+++ fail2ban_ssh.sh
@@ -8,3 +8,10 @@ cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 echo -e "[sshd]\nenabled = true\nport = 3022\nfilter = sshd\nmaxretry = 5\nbantime = 2h" >> /etc/fail2ban/jail.local
 
 systemctl enable fail2ban && systemctl start fail2ban
+
+# Keepalive
+echo -e "#!/bin/bash\nsystemctl is-active --quiet fail2ban || systemctl restart fail2ban" > /root/scripts/restart_fail2ban.sh
+chmod +x /root/scripts/restart_fail2ban.sh
+(crontab -l 2>/dev/null; echo "0 0 * * * /root/scripts/restart_fail2ban.sh") | crontab -
+
+
