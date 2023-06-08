5a0bbeb15e36d8585b8312f00b5e91ad4726cbf1
David <81330265+Dyarven@users.noreply.github.com>
2023-06-08 11:24:29 +0200

    Update arm64_zomboid_server.sh

--- arm64_zomboid_server.sh
+++ arm64_zomboid_server.sh
@@ -4,7 +4,7 @@
 # It will create the necessary files and folders and ask you to set up an admin password to access the server.
 # After that you can just shut it down and use "systemctl enable zomboid-server" and "systemctl start zomboid-server" to run it. 
 # Default server takes 8GB of RAM.
-# This script asumes you opened ports 16261 and 16262 tcp/udp on your firewall
+# This script asumes you opened ports 16261 and 16262 tcp/udp on your firewall and forwarded in the oracle cloud console for your vm instance
 
 # install java / dependencies
 sudo apt update && sudo apt full-upgrade -y
