a9bb5116adbd0fa7386a4df1c9a4afb5f32bb6ae
David <81330265+Dyarven@users.noreply.github.com>
2023-06-08 11:14:22 +0200

    Update arm64_zomboid_server.sh

--- arm64_zomboid_server.sh
+++ arm64_zomboid_server.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+###### IMPORTANT ######
+# The first time you run the server it must be manually launching it from start-server.sh.
+# It will create the necessary files and folders and ask you to set up an admin password to access the server.
+# After that you can just shut it down and use "systemctl enable zomboid-server" and "systemctl start zomboid-server" to run it.
+# This script asumes you opened ports 16261 and 16262 tcp/udp on your firewall
 
-# java / dependencies
+# install java / dependencies
 sudo apt update && sudo apt full-upgrade -y
 sudo apt install -y git build-essential cmake gcc-arm-linux-gnueabihf openjdk-19-jdk
 
-# enable armhf architecture / install dependencies
+# enable arm hard float architecture / install dependencies
 sudo dpkg --add-architecture armhf && sudo apt update
 sudo apt install -y libc6:armhf libncurses5:armhf libstdc++6:armhf
 
@@ -39,14 +44,14 @@ sudo mkdir -p /opt/zomboid-server/
 sudo chown -R $USER:$USER /opt/zomboid-server/
 ./steamcmd.sh +@sSteamCmdForcePlatformType linux +login anonymous +force_install_dir /opt/zomboid-server/ +app_update 380870 validate +quit > /dev/null
 
-# zomboid service file so we can use systemd
+# sets up zomboid as a systemd service
 sudo tee /etc/systemd/system/zomboid-server2.service > /dev/null <<EOL
 [Unit]
 Description=Zomboid
 After=network.target
 
 [Service]
-User=root
+User=$(whoami)
 WorkingDirectory=/opt/zomboid-server
 ExecStart=/opt/zomboid-server/start-server.sh
 Restart=always
@@ -55,3 +60,4 @@ Restart=always
 WantedBy=multi-user.target
 EOL
 
+systemctl daemon-reload
