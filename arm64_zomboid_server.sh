113df76fef1bd6e9bf9f2b7ad3b65aef53673004
David <81330265+Dyarven@users.noreply.github.com>
2023-06-08 17:35:01 +0200

    Update arm64_zomboid_server.sh

--- arm64_zomboid_server.sh
+++ arm64_zomboid_server.sh
@@ -5,6 +5,8 @@
 # After that you can just shut it down and use "systemctl enable zomboid-server" and "systemctl start zomboid-server" to run it. 
 # Default server takes 8GB of RAM.
 # This script asumes you opened ports 16261 and 16262 tcp/udp on your firewall and forwarded in the oracle cloud console for your vm instance
+# Notice we are using /opt/zomboid-server as a dir but zomboid's starting script will generate files in /root. It's split in two directories but it works and you can set startup parameters.
+# This is a workaround I found for the "couldn't determine 32/64 bit of java" issues.
 
 # install java / dependencies
 sudo apt update && sudo apt full-upgrade -y
