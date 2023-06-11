e9cf845e7a174e3fe463dc7435c248dd07f50ab8
David <81330265+Dyarven@users.noreply.github.com>
2023-06-11 22:44:04 +0200

    Update fail2ban_setup.sh

--- fail2ban_ssh.sh
+++ fail2ban_ssh.sh
@@ -2,7 +2,7 @@
 apt update && apt install -y fail2ban logwatch
 
 # Configuration
-cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+echo -n > /etc/fail2ban/jail.local
 
 # Modify specific options in jail.local
 echo -e "[sshd]\nenabled = true\nport = 3022\nfilter = sshd\nmaxretry = 5\nbantime = 2h" >> /etc/fail2ban/jail.local
