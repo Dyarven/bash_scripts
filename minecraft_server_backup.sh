204d4deba294e4aee371cf0584196a248bfb1ffb
David <81330265+Dyarven@users.noreply.github.com>
2024-09-08 00:57:39 +0200

    Add files via upload

---
+++ minecraft_server_backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WORLD_DIR="/opt/minecraft-server/world"
+BACKUP_DIR="/opt/minecraft-server/backups/current_world"
+LOG_DIR="/opt/minecraft-server/logs"
+DATE=$(date +"%d-%m-%Y_%H-%M-%S")
+
+mkdir -p "$BACKUP_DIR"
+
+zip -r "$BACKUP_DIR/world_backup_$DATE.zip" "$WORLD_DIR"
+
+# Delete backups older than 14 days
+find "$BACKUP_DIR" -type f -name "*.zip" -mtime +14 -exec rm {} \;
+
+# Delete logs older than 14 days
+find "$LOG_DIR" -type f -name "*.gz" -mtime +14 -exec rm {} \;
