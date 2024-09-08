1dc8170e35a86655a5082702b4f0bc7bdb838d54
David <81330265+Dyarven@users.noreply.github.com>
2024-09-08 16:08:20 +0200

    Update minecraft_server_backup.sh

--- minecraft_server_backup.sh
+++ minecraft_server_backup.sh
@@ -7,7 +7,8 @@ DATE=$(date +"%d-%m-%Y_%H-%M-%S")
 
 mkdir -p "$BACKUP_DIR"
 
-zip -r "$BACKUP_DIR/world_backup_$DATE.zip" "$WORLD_DIR"
+cd "$WORLD_DIR"
+zip -r "$BACKUP_DIR/world_backup_$DATE.zip" ./*
 
 # Delete backups older than 14 days
 find "$BACKUP_DIR" -type f -name "*.zip" -mtime +14 -exec rm {} \;
